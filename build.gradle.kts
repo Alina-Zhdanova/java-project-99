534094f293b1e2825b312c2058b6a72f147f2825
Alina Zhdanova <gdanova98@gmail.com>
2025-10-14 00:38:00 +0400

    Edited Dockerfile.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
-	checkstyle
+//	checkstyle
 	id("org.sonarqube") version "6.3.1.5724"
 	jacoco
 	application
